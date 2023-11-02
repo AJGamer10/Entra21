31865bfbcddf9b6c6ad9534ea9b0d10418371f1f
Abner Eger <abner.juda.eger.santana@gmail.com>
2023-11-01 22:19:57 -0300

    feat:

---
+++ Js/Aula/static/js/kanban.js
@@ -0,0 +1,56 @@
+$(document).ready(() => {
+    // Selecionando os elementos
+    const $form = $('#addTaskForm');
+    const $descriptionInput = $('#descriptionInput');
+    const $boardSelect = $('#boardSelect');
+    const myModal = new bootstrap.Modal('#addTaskModal')
+    const $modalCloseButton = $('.modal-close-button');
+    $modalCloseButton.click(() => {
+        myModal.hide();
+        $form.removeClass('was-validated')
+    })
+    const createItemButton = (iconClass, btnClasses, clickHandler) => {
+        const $button = $('<button></button>').addClass(btnClasses);
+        const $icon = $('<i></i>').addClass(iconClass);
+        $button.append($icon);
+        $button.click(clickHandler);
+        return $button
+    }
+    const addTaskToBoard = (description, board) => {
+        const $newTask = $('<div></div>').addClass('kanban-item')
+        const $taskText = $('<span></span>').text(description)
+        const $editButton = createItemButton('bi bi-pencil', "btn btn-warning btn-sm", () => {
+            const editedText = prompt('Nova descrição', description)
+            if (editedText) {
+                $taskText.text(editedText)
+            }
+        })
+        const $deleteButton = createItemButton('bi bi-x', 'btn btn-danger btn-sm', () => {
+            $newTask.remove()
+        })
+        const $buttonsContainer = $('<div></div>').addClass('d-flex column-gap-2');
+        $buttonsContainer.append($editButton, $deleteButton)
+        $newTask.append($taskText, $buttonsContainer)
+        $(`#${board}`).append($newTask)
+    }
+    // Evento para a submissão do formulário
+    $form.submit(event => {
+        // Cancelar o comportamento padrão do envio no formulário (Recarregar a página)
+        event.preventDefault();
+
+        // Verificar se o formlário é válido
+        if ($form[0].checkValidity()) {
+            // Adicionar a tarefa
+            addTaskToBoard($descriptionInput.val(), $boardSelect.val())
+            $form[0].reset()
+            myModal.hide()
+            $form.removeClass('was-validated')
+        } else {
+            $form.addClass('was-validated')
+        }
+    })
+
+    $('.kanban-column').sortable({
+        connectWith: '.kanban-column', cursor: 'grab'
+    }).disableSelection()
+})
