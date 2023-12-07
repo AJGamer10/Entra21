df6ef5b536d01d407d96fb7c3575405088749a8d
Abner Eger <abner.juda.eger.santana@gmail.com>
2023-12-06 21:36:41 -0300

    feat: :sparkles: termino da aula 1

--- POO/Aula/Aula1/conta.py
+++ POO/Aula/Aula1/conta.py
@@ -1,4 +1,5 @@
 """Implementação da Conta."""
+from __future__ import annotations
 
 
 class Conta:
@@ -10,6 +11,7 @@ class Conta:
         saldo (float): Saldo da conta.
         limite (float): Limite da conta.
     """
+    quantidade_contas = 0 # Atributo estático -> pertence a classe e não ao objeto
 
     def __init__(self, numero: str, titular: str):
         # Realizando validação do número da conta
@@ -20,7 +22,13 @@ class Conta:
         self.__numero = numero
         self.titular = titular # Está usando o @titular.setter para definir o valor
         self.__limite = 100
-        self.__saldo = 0    
+        self.__saldo = 0
+
+        Conta.quantidade_contas += 1
+
+    # Formata a visualização (print) do objeto
+    def __str__(self):
+        return f"Titular: {self.__titular} | Saldo: {self.__saldo} | Limite: {self.__limite}"
 
     @property
     def titular(self):
@@ -49,6 +57,51 @@ class Conta:
             valor (float): Valor do deposito.
         """
         self.__saldo += valor
+    
+    def sacar(self, valor: float) -> bool:
+        """Saca valor da conta se o saldo + limite for maior ou igual ao valor do saque.
+        
+        Args:
+            valor (float): Valor saque.
+        
+        Returns:
+            True se for bem-sucedido, False caso contrário.
+        """
+        if (self.__saldo + self.__limite) < valor:
+            print("Saldo indisponivel para realizar a operação.")
+            return False
+        
+        if self.__saldo < valor:
+            self.__limite -= valor - self.__saldo
+            self.__saldo = 0
+        else:
+            self.__saldo -= valor
+        
+        return True
+    
+    def tranferir(self, valor: float, conta_destino: Conta) -> None:
+        """Tranfere o valor de uma conta para outra se o (saldo + limite)
+        for maior ou igual ao valor do saque.
+
+        Args:
+            valor (float): Valor da tranferência.
+            conta_destino (Conta): Conta de destino da transferência.
+        """
+        if (self.sacar(valor)):
+            conta_destino.depositar(valor)
+            print("Transferência realizada com sucesso.")
+
+    @staticmethod
+    def verifica_numero_conta(numero: str) -> bool:
+        """Verifica se o número da conta é válido.
+
+        Args:
+            numero (str): Número da conta.
+
+        Returns:
+            True caso o número da conta seja válido, False caso contrário
+        """
+        return len(numero) == 9
 
 
 if __name__ == "__main__":
@@ -63,3 +116,22 @@ if __name__ == "__main__":
     print(f'Valor antes do deposito: {conta_william.saldo}')
     conta_william.depositar(100_000_000)
     print(f'Valor antes do deposito: {conta_william.saldo}')
+    conta_william.sacar(100_000_101)
+    conta_william.sacar(1000)
+    print(conta_william)
+    conta_william.sacar(99999050)
+    print(conta_william)
+
+    conta_marcos = Conta('123467282', "marcos")
+    print(conta_marcos)
+
+    conta_william.tranferir(20, conta_marcos)
+    print(conta_william)
+    print(conta_marcos)
+
+    print(Conta.quantidade_contas)
+
+    if Conta.verifica_numero_conta('256173852'):
+        print('Número da conta é válido!')
+    else:
+        print('Número da conta é inválido!')
