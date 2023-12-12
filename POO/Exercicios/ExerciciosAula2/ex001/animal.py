3f6cb52d26e17dc73fa53cd1f81d5323a794f969
Abner Eger <abner.juda.eger.santana@gmail.com>
2023-12-12 16:32:07 -0300

    feat: :sparkles: começando ex 001 Aula 2

---
+++ POO/Exercicios/ExerciciosAula2/ex001/animal.py
@@ -0,0 +1,9 @@
+"""
+1) Implemente uma superclasse Animal com métodos como emitir_som() e mover(). Crie 
+subclasses como Cachorro, Gato, Pássaro. Sobrescreva os métodos para representar o 
+nome específico de cada animal.
+"""
+
+
+class Animal:
+    """Animal representa um animal."""
