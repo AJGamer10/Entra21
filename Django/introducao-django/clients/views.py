eca81eebbfa389aec998566ae1d3b2d81354a44b
Abner Eger <abner.juda.eger.santana@gmail.com>
2023-12-20 23:21:23 -0300

    feat: :sparkles: Começando a criar páginas com django

--- Django/introducao-django/clients/views.py
+++ Django/introducao-django/clients/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Client
+from .forms import ClientForm
 
 # Create your views here.
 def index(request):
@@ -24,12 +25,63 @@ def detail(request, client_id):
 
 
 def create(request):
-    return HttpResponse("Criação")
+    # Metodo POST -> Enviando o formulário de cadastro
+    if request.method == 'POST':
+        form = ClientForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("index"))
+        
+        context = {
+            "form": form
+        }
+
+        return render(request, "create.html", context)
+
+    # Metodo GET -> Abrir a página para cadastrar
+    form = ClientForm()
+
+    context = {
+        "form": form
+    }
+
+    return render(request, "create.html", context)
 
 
 def update(request, client_id):
-    return HttpResponse(f"Edição: {client_id}")
+    client = get_object_or_404(Client, pk=client_id)
+
+    # POST
+    if request.method == "POST":
+        form = ClientForm(request.POST, instance=client)
+
+        if form.is_valid():
+            form.save()
+            # return redirect("update", client_id=client.id)
+            return redirect("index")
+        
+        context = {
+            "form": form,
+            "client": client
+        }
+
+        return render(request, "update.html", context)
+
+
+    # GET
+    form = ClientForm(instance=client)
+
+    context = {
+        "form": form,
+        "client": client
+    }
+
+    return render(request, 'update.html', context)
 
 
 def delete(request, client_id):
-    return HttpResponse(f"Remover: {client_id}")
+    client =  get_object_or_404(Client, pk=client_id)
+    client.delete()
+
+    return  HttpResponseRedirect(reverse("index"))
